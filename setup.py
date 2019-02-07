2bcd2205f43424ed14d82c16aa14b2e1a3f28a65
Zach Schuermann <zachary.zvs@gmail.com>
2019-02-07 00:10:41 -0600

    package fixes

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sudoku-solver",
-    version="0.0.1",
+    name="sudoku-solve",
+    version="0.0.3",
     author="Zach Schuermann",
     author_email="zachary.schuermann@gmail.com",
     description="A small sudoku solver",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    scripts=["sudoku-solver/sudoku.py"],
+    scripts=["sudoku_solve/sudoku.py"],
 )
